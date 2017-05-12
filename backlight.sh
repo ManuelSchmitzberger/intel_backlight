1ce3f0b256bffc698e19f60f9c0acddf21ec35b3
Manuel Schmitzberger <manuel.schmitzberger@live.com>
2017-05-12 12:06:06 +0200

    Update backlight.sh
    Update for my own usage

--- backlight.sh
+++ backlight.sh
@@ -1,45 +1,21 @@
-
 #!/bin/bash
 
 folder="/sys/class/backlight/intel_backlight"
 
-cd "$folder"
-
-value=3000
 max=4882
 min=100
 
-if (($EUID != 0))
-then
-	echo "You do not have required previlages to run the script!!!"
-	exit 1
+currentValue=$(cat $folder/brightness)
+addValue=$1
+setValue=$(($currentValue + $addValue))
+
+echo $setValue
+
+if (($setValue < 100)); then
+ setValue=100;
+elif (($setValue > 4000)); then
+  setValue=4000;
 fi
 
-while true
-do
-	read -n3 -s  key
-	echo -n "$key" | grep "\[B" 
-	if [ "$?" -eq 0 ]
-	then
-		if (($value < 100))
-		then
-			value=100
-			continue
-		fi
-		echo $value
-		echo -n $value > brightness  
-		value=$[$value-100]
-	fi
-	echo -n "$key" | grep "\[A"  
-	if [ "$?" -eq 0 ]
-	then
-		if (($value > 4000))
-		then
-			continue
-		fi
-		echo $value
-		echo -n $value > brightness  
-		value=$[$value+100]
-	fi
-	clear
-done
+echo $folder/brightness
+echo -n $setValue > $folder/brightness
